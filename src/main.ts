40e3928de12b62af5bb0c5d74f00e7227dcda8ff
Matt Rich <matt.rich@e2log.com>
2025-04-13 16:51:54 -0700

    updates

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://bfb-frontend-production.up.railway.app'],
+    origin: ['http://localhost:5173', 'https://budgetfriendlybudget.com', 'https://www.budgetfriendlybudget.com'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
